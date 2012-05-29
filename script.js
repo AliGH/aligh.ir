30e97ae8d5065402389e667b43f4e9fe130e4f2f
aligh <aghasizadeh70@gmail.com>
2012-05-29 12:42:05 +0430

    add contact-me and  js validate form

--- script.js
+++ script.js
@@ -66,3 +66,51 @@ $(function(){
 	});
 
 });
+
+window.onload = function(){
+
+	var myform = document.forms.item(0);
+	
+	myform.onsubmit = function(){
+		
+		var ret = true;
+		
+		for(var i=0;i<myform.elements.length;i++){
+			if(myform.elements.item(i).className!='ok'){
+				ret = false;
+				break;
+			}
+		}
+		
+		return ret;
+	}
+	
+	for (var i=0;i<myform.elements.length;i++){
+		
+		var inp = myform.elements.item(i);
+		
+		if (inp.type!='text' && inp.type!='textarea'){
+			continue;
+		}
+		
+		inp.onfocus = function(){
+			if (this.value == this.defaultValue){
+				this.value = '';
+			}
+		}
+
+		inp.onblur = function(){
+			
+			if (this.value!=''){
+				this.className='ok';
+			}else{
+				this.className='nok'
+			}
+			
+			if (this.value == ''){
+				this.value = this.defaultValue;
+			}
+			
+		}
+	}
+}
